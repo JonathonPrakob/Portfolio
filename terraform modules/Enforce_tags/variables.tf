087b76cfaf37564188f9d915acb08ca6e26f38ea
JonathonPrakob <114543294+JonathonPrakob@users.noreply.github.com>
2023-02-15 15:50:41 -0500

    Delete terraform modules/Enforce_tags directory

--- terraform modules/Enforce_tags/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "var_management_group" {
-  type    = string
-  default = "RootManagementGroup"
-}
-variable "var_not_scopes" {
-  type    = list
-  default = []
-  description = "use '/subscriptions/(enter subscription ID)/resourceGroups/(enter resource group id)' to set scope(s)"
-}
