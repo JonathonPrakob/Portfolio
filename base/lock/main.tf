4fc828bde65481690bd3273fdd550a46b5de73ea
JonathonPrakob <114543294+JonathonPrakob@users.noreply.github.com>
2023-05-11 15:08:48 -0400

    Delete base directory

--- base/lock/main.tf
+++
@@ -1,9 +0,0 @@
-data "azurerm_subscription" "current" {
-}
-
-resource "azurerm_management_lock" "foo" {
-  name       = var.lock_level
-  scope      = var.scope
-  lock_level = var.lock_level
-  notes      = var.notes
-}
